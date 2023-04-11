9906ce423fcfc32e0771943afb1f1c31d8716425
stoyanK7 <stoyank127@gmail.com>
2023-04-11 19:44:08 +0200

    Fix bug in password hash check

--- api/src/main/gateway/auth.py
+++ api/src/main/gateway/auth.py
@@ -58,7 +58,7 @@ def authenticate_user(db: Session, username: str, password: str):
     user = crud.get_user(db, username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user.password):
         return False
     return user
 
