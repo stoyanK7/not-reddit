05287436e5c86a280c22ae3950ebe0e77596f506
stoyanK7 <stoyank127@gmail.com>
2023-04-12 12:44:38 +0200

    Move to Azure AD as authentication service provider

--- api/src/main/gateway/main.py
+++ api/src/main/gateway/main.py
@@ -1,34 +1,28 @@
-from typing import Annotated
+from fastapi import Depends, FastAPI
 
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-from starlette.status import HTTP_401_UNAUTHORIZED
-
-from src.main.database import get_db
-from src.main.gateway.auth import create_access_token, authenticate_user
-from src.main.gateway.schema import Token
+from src.main.gateway.config import configure_cors, azure_scheme, load_openid_config_on_startup
 from src.main.gateway.routers.user import router as user_router
 from src.main.gateway.routers.comment import router as comment_router
 from src.main.gateway.routers.post import router as post_router
 from src.main.gateway.routers.subreddit import router as subreddit_router
 from src.main.gateway.routers.vote import router as vote_router
+from src.main.gateway.settings import settings
 
-app = FastAPI()
+app = FastAPI(
+    swagger_ui_oauth2_redirect_url='/oauth2-redirect',
+    swagger_ui_init_oauth={
+        'usePkceWithAuthorizationCodeGrant': True,
+        'clientId': settings.OPENAPI_CLIENT_ID,
+    },
+)
+
+configure_cors(app)
+load_openid_config_on_startup(app)
 
 
-@app.post("/login", response_model=Token)
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(data={"sub": user.username})
-    return {"access_token": access_token, "token_type": "bearer"}
+@app.get("/", dependencies=[Depends(azure_scheme)])
+async def root():
+    return {"message": "Hello World"}
 
 
 app.include_router(user_router)
--- api/src/main/gateway/settings.py
+++ api/src/main/gateway/settings.py
@@ -1,21 +1,23 @@
-from pydantic import BaseSettings
+from typing import Union
+from pydantic import AnyHttpUrl
 
+from src.main.settings import DatabaseSettings
 from src.main.env import get_env
 
 
-class Settings(BaseSettings):
-    DB_DIALECT = get_env("DB_DIALECT")
-    DB_USER = get_env("DB_USER")
-    DB_PASSWORD = get_env("DB_PASSWORD")
-    DB_NAME = get_env("DB_NAME")
-    DB_HOST = get_env("DB_HOST")
-    DB_PORT = get_env("DB_PORT")
-
+class Settings(DatabaseSettings):
+    # Paths
     COMMENT_SERVICE_URL = get_env("COMMENT_SERVICE_URL")
     POST_SERVICE_URL = get_env("POST_SERVICE_URL")
     SUBREDDIT_SERVICE_URL = get_env("SUBREDDIT_SERVICE_URL")
     USER_SERVICE_URL = get_env("USER_SERVICE_URL")
     VOTE_SERVICE_URL = get_env("VOTE_SERVICE_URL")
+    # Auth
+    APP_CLIENT_ID = get_env("APP_CLIENT_ID")
+    OPENAPI_CLIENT_ID = get_env("OPENAPI_CLIENT_ID")
+    SECRET_KEY = get_env("SECRET_KEY")
+    # CORS
+    CORS_ORIGINS: list[Union[str, AnyHttpUrl]] = ['http://localhost:8000']
 
 
 settings = Settings()
