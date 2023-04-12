c9707c36da522ea5bdb562dc0fc825dd99561704
stoyanK7 <stoyank127@gmail.com>
2023-04-12 22:31:25 +0200

    Add utility function for checking for environment variables

---
+++ .ci/util.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+function checkForVariable {
+  VAR_NAME=$1
+  if [ ! -v "$VAR_NAME" ]; then
+    echo "[Error] Define $1 environment variable"
+    exit 1
+  fi
+
+  VAR_VALUE="${!VAR_NAME}"
+  if [ -z "$VAR_VALUE" ]; then
+    echo "[Error] Set not empty value to $1 environment variable"
+    exit 1
+  fi
+}
