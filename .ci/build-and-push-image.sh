b3f436466e87a86cbee5b248e0ed995f62408a67
stoyanK7 <stoyank127@gmail.com>
2023-04-12 22:31:54 +0200

    Move docker image build and push to a script

---
+++ .ci/build-and-push-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+source ./.ci/util.sh
+
+checkForVariable REGISTRY_LOGIN_SERVER
+checkForVariable SERVICE
+
+cd api || exit
+
+IMAGE_NAME="$REGISTRY_LOGIN_SERVER/$SERVICE-api:latest"
+docker build . \
+  -t "$IMAGE_NAME" \
+  -f src/main/"$SERVICE"/Dockerfile
+docker push "$IMAGE_NAME"
