527181b1bcffc6a7de9c590b9bf371ff00b4f2a9
stoyanK7 <stoyank127@gmail.com>
2023-04-13 23:48:09 +0200

    Update DatabaseSettings to have explicit type

--- api/src/main/settings.py
+++ api/src/main/settings.py
@@ -4,12 +4,12 @@ from src.main.env import get_env
 
 
 class DatabaseSettings(BaseSettings):
-    DB_DIALECT = get_env("DB_DIALECT")
-    DB_USER = get_env("DB_USER")
-    DB_PASSWORD = get_env("DB_PASSWORD")
-    DB_NAME = get_env("DB_NAME")
-    DB_HOST = get_env("DB_HOST")
-    DB_PORT = get_env("DB_PORT")
+    DB_DIALECT: str = get_env("DB_DIALECT")
+    DB_USER: str = get_env("DB_USER")
+    DB_PASSWORD: str = get_env("DB_PASSWORD")
+    DB_NAME: str = get_env("DB_NAME")
+    DB_HOST: str = get_env("DB_HOST")
+    DB_PORT: str = get_env("DB_PORT")
 
 
 settings = DatabaseSettings()
