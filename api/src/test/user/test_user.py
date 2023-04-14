065df589c4d49a523266de09b0aefe1dfd189a0d
stoyanK7 <stoyank127@gmail.com>
2023-04-14 15:33:50 +0200

    Remove test for checking if passwords are hashed

--- api/src/test/user/test_user.py
+++ api/src/test/user/test_user.py
@@ -10,15 +10,3 @@ def test_create_user(client, session, mock_user, remove_json_fields):
     response = client.post("/user", json=body)
 
     assert response.status_code == HTTP_201_CREATED
-
-
-def test_create_user_password_hashed(client, session, mock_user, remove_json_fields):
-    """Assert that user is created and that password is hashed."""
-    body = mock_user
-
-    response = client.post("/user", json=body)
-    user = session.query(UserModel).filter(UserModel.username == body["username"]).first()
-
-    assert response.status_code == HTTP_201_CREATED
-    assert user.password != body["password"]
-    assert user.password.startswith("$2")
