09320cf2c0c78af7b26f0f65c32febe291d2400c
stoyanK7 <stoyank127@gmail.com>
2023-04-14 15:35:02 +0200

    Remove unused import

--- api/src/test/user/test_user.py
+++ api/src/test/user/test_user.py
@@ -1,7 +1,5 @@
 from starlette.status import HTTP_201_CREATED
 
-from src.main.user.model import User as UserModel
-
 
 def test_create_user(client, session, mock_user, remove_json_fields):
     """Assert that user is created."""
