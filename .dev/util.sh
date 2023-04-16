db42526be9158d17bc34216be34575134008f6f4
stoyanK7 <stoyank127@gmail.com>
2023-04-16 19:09:28 +0200

    Update util function to assert we are in api directory

--- .dev/util.sh
+++ .dev/util.sh
@@ -2,7 +2,7 @@
 
 function assert_in_api_dir {
   # Assert that the script is being run from the api directory.
-  if [[ ! -d .venv ]]; then
+  if [[ ! $(pwd) =~ api$ ]]; then
     echo "Please run this script from the api directory."
     exit 1
   fi
