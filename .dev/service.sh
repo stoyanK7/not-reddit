97494f441582d9bd07176dc1ac28dc035cc09cc2
stoyanK7 <stoyank127@gmail.com>
2023-04-16 19:09:12 +0200

    Update script to spin up a service locally

---
+++ .dev/service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+source util.sh
+
+assert_in_api_dir
+assert_venv_active
+SERVICE=$1
+
+if [[ -z "$SERVICE" ]]; then
+  echo "service is not set"
+  echo "Usage: $BASH_SCRIPT <service>"
+  exit 1
+fi
+
+if [[ -z "$STARTING_PORT" ]]; then
+  export STARTING_PORT=8080
+fi
+
+echo "Starting $SERVICE service on port $STARTING_PORT."
+gnome-terminal --tab -- uvicorn src.main."$SERVICE".main:app --reload --port "$STARTING_PORT"
+STARTING_PORT=$((STARTING_PORT + 1))
