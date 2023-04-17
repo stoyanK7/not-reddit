cdd4cc7448ca34386f3ee3bfe243ee1f65ea5a63
stoyanK7 <stoyank127@gmail.com>
2023-04-17 19:13:29 +0200

    Make username a primary key

--- api/src/main/user/model.py
+++ api/src/main/user/model.py
@@ -6,6 +6,6 @@ from src.main.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True, primary_key=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
