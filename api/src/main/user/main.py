b7b2f57ee1aa68f813ffbd7036eb31e55416b7d9
stoyanK7 <stoyank127@gmail.com>
2023-04-19 19:43:03 +0200

    Add extra checks to endpoints to handle misuse cases

--- api/src/main/user/main.py
+++ api/src/main/user/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, Request
+import jwt
+from fastapi import FastAPI, Depends, HTTPException, Request
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_401_UNAUTHORIZED, HTTP_409_CONFLICT
 from sqlalchemy.orm import Session
 
 from src.main.database import get_db, engine
@@ -13,24 +15,51 @@ app = FastAPI()
 configure_cors(app)
 
 
-@app.post("/", status_code=201)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # TODO: check token an ensure email is the same as the one in the token
-    # TODO: check if username and email are unique
-    crud.create_user(db=db, user=user)
+@app.post("/", status_code=HTTP_201_CREATED, dependencies=[Depends(azure_scheme)])
+def create_user(request: Request, body: UserCreate, db: Session = Depends(get_db)):
+    assert_is_jwt_email_same_as_provided_email(body.email, request=request)
+    assert_is_username_and_email_not_taken(body.username, body.email, db=db)
+
+    crud.create_user(db=db, user=body)
     return
 
 
-@app.get("/{username}", status_code=200, dependencies=[Depends(azure_scheme)])
+@app.get("/{username}", status_code=HTTP_200_OK, dependencies=[Depends(azure_scheme)])
 def get_user(username: str, db: Session = Depends(get_db)):
     return crud.get_user(db=db, username=username)
 
 
-@app.post("/registered", status_code=200)
+@app.post("/registered", status_code=HTTP_200_OK, dependencies=[Depends(azure_scheme)])
 def check_if_registered(request: Request, body: UserCheckIfRegistered,
                         db: Session = Depends(get_db)):
-    # TODO: check if request is made by the same user
-    print(request.headers)
-    print('hello')
-    registered = crud.check_if_registered(db=db, email=body.email)
+    assert_is_jwt_email_same_as_provided_email(body.email, request=request)
+
+    registered = crud.get_user_by_email(db=db, email=body.email) is not None
     return {"registered": registered}
+
+
+def assert_is_jwt_email_same_as_provided_email(provided_email: str, request: Request):
+    jwt_token = extract_token(request.headers['Authorization'])
+    decoded = jwt.decode(jwt_token, options={"verify_signature": False})
+    is_jwt_email_same_as_body_email = decoded['preferred_username'] == provided_email
+
+    if not is_jwt_email_same_as_body_email:
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized"
+        )
+
+
+def assert_is_username_and_email_not_taken(username: str, email: str, db: Session):
+    is_username_or_email_taken = crud.get_user_by_username_or_email(db=db, username=username,
+                                                                    email=email)
+    if is_username_or_email_taken:
+        raise HTTPException(
+            status_code=HTTP_409_CONFLICT,
+            detail="Username or email already taken"
+        )
+
+
+def extract_token(authorization_header: str):
+    """Extracts the token from the authorization header by removing the 'Bearer ' part."""
+    return authorization_header[7:]
--- api/src/test/user/test_user.py
+++ api/src/test/user/test_user.py
@@ -1,10 +1,52 @@
 from starlette.status import HTTP_201_CREATED
+import jwt
+
+from src.main.user.model import User as UserModel
 
 
 def test_create_user(client, session, mock_user, remove_json_fields):
     """Assert that user is created."""
     body = mock_user
+    jwt_token = jwt.encode({"preferred_username": body["email"]}, "secret", algorithm="HS256")
 
-    response = client.post("/", json=body)
+    response = client.post("/", json=body, headers={"Authorization": f"Bearer {jwt_token}"})
 
     assert response.status_code == HTTP_201_CREATED
+
+
+def test_create_user_with_different_jwt_email_and_body_email(client, session, mock_user):
+    """Assert that user is not created when jwt email and body email are different."""
+    body = mock_user
+    jwt_token = jwt.encode({"preferred_username": "different_email"}, "secret", algorithm="HS256")
+
+    response = client.post("/", json=body, headers={"Authorization": f"Bearer {jwt_token}"})
+
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Unauthorized"}
+
+
+def test_create_user_with_taken_username(client, session, mock_user):
+    """Assert that user is not created when username is taken."""
+    body = mock_user
+    jwt_token = jwt.encode({"preferred_username": body["email"]}, "secret", algorithm="HS256")
+    session.add(UserModel(**body))
+    session.commit()
+
+    response = client.post("/", json=body, headers={"Authorization": f"Bearer {jwt_token}"})
+
+    assert response.status_code == 409
+    assert response.json() == {"detail": "Username or email already taken"}
+
+
+def test_create_user_with_taken_email(client, session, mock_user):
+    """Assert that user is not created when email is taken."""
+    body = mock_user
+    jwt_token = jwt.encode({"preferred_username": body["email"]}, "secret", algorithm="HS256")
+    session.add(UserModel(**body))
+    session.commit()
+
+    body["username"] = "different_username"
+    response = client.post("/", json=body, headers={"Authorization": f"Bearer {jwt_token}"})
+
+    assert response.status_code == 409
+    assert response.json() == {"detail": "Username or email already taken"}
