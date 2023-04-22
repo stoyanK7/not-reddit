4ad070653d07649cb40c383052e04f5dc3099f48
stoyanK7 <stoyank127@gmail.com>
2023-04-22 14:59:35 +0200

    Implement email sending upon registration

--- api/src/main/user/main.py
+++ api/src/main/user/main.py
@@ -3,18 +3,20 @@ from starlette.status import HTTP_201_CREATED, HTTP_200_OK
 from sqlalchemy.orm import Session
 
 from src.main.database import get_db, engine
+from src.main.user.lifespan import lifespan
 from src.main.user.model import Base
 from src.main.user import crud
 from src.main.user.schema import UserCreate
 from src.main.user.schema import UserCheckIfRegistered
 from src.main.auth_config import configure_cors, azure_scheme
 from src.main.user.util import assert_is_jwt_email_same_as_provided_email, \
-    assert_is_username_and_email_not_taken, assert_is_user_exists
+    assert_is_username_and_email_not_taken, assert_is_user_exists, \
+    send_successful_registration_email
 from random_username.generate import generate_username
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 configure_cors(app)
 
 
@@ -33,6 +35,7 @@ def create_user(request: Request, body: UserCreate, db: Session = Depends(get_db
     assert_is_username_and_email_not_taken(username=new_username, email=body.email, db=db)
 
     crud.create_user(db=db, username=new_username, email=body.email)
+    send_successful_registration_email(email=body.email)
     return
 
 
--- api/src/main/user/util.py
+++ api/src/main/user/util.py
@@ -1,13 +1,18 @@
-import jwt
+import json
+
+from jwt import decode
 from fastapi import HTTPException, Request
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+
 from src.main.user.crud import get_user_by_username_or_email
+from src.main.user.rabbitmq import channel
+from src.main.user.settings import settings
 
 
 def assert_is_jwt_email_same_as_provided_email(provided_email: str, request: Request):
     jwt_token = extract_token(request.headers['Authorization'])
-    decoded = jwt.decode(jwt_token, options={"verify_signature": False})
+    decoded = decode(jwt_token, options={"verify_signature": False})
     is_jwt_email_same_as_body_email = decoded['preferred_username'] == provided_email
 
     if not is_jwt_email_same_as_body_email:
@@ -38,3 +43,11 @@ def assert_is_user_exists(user):
 def extract_token(authorization_header: str):
     """Extracts the token from the authorization header by removing the 'Bearer ' part."""
     return authorization_header[7:]
+
+
+def send_successful_registration_email(email: str):
+    body = json.dumps({
+        "recipients": [email],
+        "content_topic": "success_register"
+    })
+    channel.basic_publish(exchange='', routing_key=settings.RABBITMQ_EMAIL_QUEUE, body=body)
