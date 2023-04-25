979746191cd81fe48dd945ad2ff76a0d5bc000a0
stoyanK7 <stoyank127@gmail.com>
2023-04-25 16:48:28 +0200

    Use access token when creating a post

--- ui/src/main/app/post/create/page.tsx
+++ ui/src/main/app/post/create/page.tsx
@@ -1,62 +1,40 @@
 "use client";
 
-import { useState } from "react";
-import { ToastContainer, toast } from 'react-toast';
-import { useMsal } from "@azure/msal-react";
-import { InteractionRequiredAuthError } from "@azure/msal-browser";
-import { redirect } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import {useState} from "react";
+import {ToastContainer, toast} from 'react-toast';
+import {useMsal} from "@azure/msal-react";
+import {InteractionRequiredAuthError} from "@azure/msal-browser";
+import {redirect} from 'next/navigation';
+import {useRouter} from 'next/navigation';
 import Link from "next/link";
+import getAccessToken from "@/app/getAccessToken";
+import buildJSONHeaders from "@/app/buildJSONHeaders";
 
 export default function PostCreatePage() {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
     const [success, setSuccess] = useState(false);
     const [status, setStatus] = useState(false);
-    const { instance, accounts } = useMsal();
-    const [id, setId] = useState("");
+    const {instance, accounts} = useMsal();
+    const [postId, setPostId] = useState("");
+    const [postType, setPostType] = useState("text");
     const router = useRouter()
 
     async function createPost() {
         if (!title) {
-            toast.info("Title is required.");
+            toast.info("Please provide a title to your post.");
             return;
         }
 
-        // console.log(instance.getActiveAccount());
-        // instance.setActiveAccount(accounts[0]);
+        const accessToken = await getAccessToken();
+        if (accessToken === null) {
+            toast.error("Failed to get your access token.");
+            return;
+        }
 
-        // instance.acquireTokenSilent(loginRequest)
-        //     .then(async tokenResponse => {
-        //         console.log("here");
-        //         const headers = new Headers();
-        //         const bearer = "Bearer " + tokenResponse.accessToken;
-        //         headers.append("Authorization", bearer);
-        //         const options = {
-        //             method: "POST",
-        //             headers: headers,
-        //             body: JSON.stringify({ title, body })
-        //         };
-
-        //         const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVICE_URL}/post`, options);
-        //         const data = await res.json();
-        //         console.log(data);
-
-        //         if (res.ok) {
-        //             toast.success("Post created successfully.");
-        //         }
-        //     }).catch(async (error) => {
-        //         console.log(error);
-        //         if (error instanceof InteractionRequiredAuthError) {
-        //             return instance.acquireTokenPopup(loginRequest);
-        //         }
-        //     })
-
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVICE_URL}/post/`, {
+        const res: Response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVICE_URL}/post/`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: buildJSONHeaders(accessToken),
             body: JSON.stringify({ title, body }),
         });
 
@@ -64,34 +42,36 @@ export default function PostCreatePage() {
             const data = await res.json();
             toast.success("Post created successfully.");
             setSuccess(true);
-            setId(data.id);
+            setPostId(data.id);
         }
     }
 
     return (
         <main className="flex w-screen h-screen justify-center items-center">
-            <div className="flex w-1/2 flex-col p-2 gap-2 shadow-reddit border border-reddit-postline">
+            <div
+                className="flex w-1/2 flex-col p-2 gap-2 shadow-reddit border border-reddit-postline">
                 <h1 className="self-center">Create a post</h1>
-                <br />
+                <br/>
                 <ToastContainer delay={8000}/>
                 <input
                     className="border border-reddit-postline rounded-sm p-2"
                     type="text"
                     placeholder="Title"
                     onChange={e => setTitle(e.currentTarget.value)}
-                    disabled={success} />
+                    disabled={success}/>
                 <textarea
                     className="border border-reddit-postline rounded-sm p-2"
                     placeholder="Text (optional)"
                     onChange={e => setBody(e.currentTarget.value)}
                     rows={6}
-                    disabled={success} />
+                    disabled={success}/>
                 <button className="p-2 rounded-sm bg-reddit-orange text-white w-full"
-                    onClick={createPost}
-                    disabled={success}
-                    hidden={success}>Post</button>
+                        onClick={createPost}
+                        disabled={success}
+                        hidden={success}>Post
+                </button>
                 {success && (
-                    <Link href={`/post/${id}`}>
+                    <Link href={`/post/${postId}`}>
                         <button className="p-2 w-full rounded-sm bg-green-400 text-white">
                             See post
                         </button>
