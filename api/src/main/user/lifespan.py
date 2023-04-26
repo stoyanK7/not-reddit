cffb08aec6be5d1cc39078dda734ef641ac5bcd9
stoyanK7 <stoyank127@gmail.com>
2023-04-26 09:37:54 +0200

    Fix rabbitmq connection in tests

--- api/src/main/user/lifespan.py
+++ api/src/main/user/lifespan.py
@@ -4,11 +4,11 @@ from fastapi import FastAPI
 from fastapi.applications import AppType
 from starlette.types import Lifespan
 
-from src.main.user.rabbitmq import rabbitmq_connection
+from src.main.user.rabbitmq import connect_to_rabbitmq, disconnect_from_rabbitmq
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> Lifespan[AppType]:
-    """Close RabbitMQ connection after app shutdown."""
+    connect_to_rabbitmq()
     yield
-    rabbitmq_connection.close()
+    disconnect_from_rabbitmq()
--- api/src/main/user/rabbitmq.py
+++ api/src/main/user/rabbitmq.py
@@ -3,9 +3,26 @@ from pika import BlockingConnection, ConnectionParameters
 from src.main.user.settings import settings
 from src.main.logger import logger
 
-rabbitmq_connection = BlockingConnection(ConnectionParameters(host=settings.RABBITMQ_HOST))
-channel = rabbitmq_connection.channel()
-logger.info("Connected to RabbitMQ.")
+channel = None
+rabbitmq_connection = None
 
-channel.queue_declare(queue=settings.RABBITMQ_EMAIL_QUEUE)
-logger.info(f"Declared queue '{settings.RABBITMQ_EMAIL_QUEUE}'.")
+
+def connect_to_rabbitmq():
+    global rabbitmq_connection
+    global channel
+
+    rabbitmq_connection = BlockingConnection(ConnectionParameters(host=settings.RABBITMQ_HOST))
+    channel = rabbitmq_connection.channel()
+    logger.info("Connected to RabbitMQ.")
+
+    channel.queue_declare(queue=settings.RABBITMQ_EMAIL_QUEUE)
+    logger.info(f"Declared queue '{settings.RABBITMQ_EMAIL_QUEUE}'.")
+
+
+def disconnect_from_rabbitmq():
+    global rabbitmq_connection
+    global channel
+
+    channel.close()
+    rabbitmq_connection.close()
+    logger.info("Disconnected from RabbitMQ.")
