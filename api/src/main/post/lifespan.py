9a4e862393d4c797c15de5ba0bd834deda524d05
stoyanK7 <stoyank127@gmail.com>
2023-04-26 18:48:12 +0200

    Add lifespan for post microservice

--- api/src/main/post/lifespan.py
+++ api/src/main/post/lifespan.py
@@ -5,12 +5,11 @@ from fastapi import FastAPI
 from fastapi.applications import AppType
 from starlette.types import Lifespan
 
-from src.main.post.rabbitmq import consume
+from src.main.post.rabbitmq import consume_messages
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> Lifespan[AppType]:
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(consume(loop))
+    asyncio.ensure_future(consume_messages(loop))
     yield
-    # await disconnect_from_rabbitmq(app)
