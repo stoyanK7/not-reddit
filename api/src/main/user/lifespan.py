b51915b11a2615be7b49434b75ae8be11691a818
stoyanK7 <stoyank127@gmail.com>
2023-04-27 18:09:04 +0200

    Add user service lifespan

--- api/src/main/user/lifespan.py
+++ api/src/main/user/lifespan.py
@@ -1,14 +1,21 @@
+import asyncio
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
 from fastapi.applications import AppType
 from starlette.types import Lifespan
 
-from src.main.user.rabbitmq import channel, rabbitmq_connection
+from src.main.database import engine, Base
+from src.main.user.service import UserService
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> Lifespan[AppType]:
+async def lifespan(app: UserService) -> Lifespan[AppType]:
+    create_database_tables()
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(app.successful_registration_amqp_publisher.prepare_connection(loop))
+    await task
     yield
-    channel.close()
-    rabbitmq_connection.close()
+
+
+def create_database_tables():
+    Base.metadata.create_all(bind=engine)
