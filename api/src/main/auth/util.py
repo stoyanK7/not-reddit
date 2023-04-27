693d7272191f2cb04ae9e7301eb42abb126072cd
stoyanK7 <stoyank127@gmail.com>
2023-04-27 18:28:38 +0200

    Remove unnecessary comments

--- api/src/main/auth/util.py
+++ api/src/main/auth/util.py
@@ -19,8 +19,6 @@ def configure_cors(app: FastAPI) -> None:
 
 
 azure_scheme = MultiTenantAzureAuthorizationCodeBearer(
-    # Endpoints should depend on this value. For example,
-    # @app.get("/", dependencies=[Depends(azure_scheme)])
     app_client_id=settings.APP_CLIENT_ID,
     scopes={
         f'api://{settings.APP_CLIENT_ID}/user_impersonation': 'user_impersonation',
@@ -32,7 +30,3 @@ azure_scheme = MultiTenantAzureAuthorizationCodeBearer(
 async def load_openid_config() -> None:
     await azure_scheme.openid_config.load_config()
     logger.info("OpenID config is loaded.")
-
-
-def load_openid_config_on_startup(app: FastAPI) -> None:
-    app.add_event_handler('startup', load_openid_config)
