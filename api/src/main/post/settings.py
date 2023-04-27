1a5122496b8b35e9b1d92ffdca64853d433bae0b
stoyanK7 <stoyank127@gmail.com>
2023-04-27 15:33:13 +0200

    Inherit post service settings from AmqpSettings

--- api/src/main/post/settings.py
+++ api/src/main/post/settings.py
@@ -1,9 +1,8 @@
-from src.main.env import get_env
+from src.main.settings import AmqpSettings
 
 
-class PostServiceSettings:
+class PostServiceSettings(AmqpSettings):
     SERVICE_PREFIX: str = "/api/post"
-    AMQP_URL: str = get_env("AMQP_URL")
 
 
 settings = PostServiceSettings()
