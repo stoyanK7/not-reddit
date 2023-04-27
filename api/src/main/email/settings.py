e251a1b2ca4e5e06bc5fe1dca66bd2e2c1070c34
stoyanK7 <stoyank127@gmail.com>
2023-04-27 20:45:26 +0200

    Add missing AMQP configuration in email service

--- api/src/main/email/settings.py
+++ api/src/main/email/settings.py
@@ -1,10 +1,10 @@
 from pydantic import BaseSettings
 
+from src.main.shared.amqp.settings import AmqpSettings
 from src.main.shared.env import get_env
 
 
-class EmailSettings(BaseSettings):
-    AMQP_URL = get_env("AMQP_URL")
+class EmailSettings(AmqpSettings):
     EMAIL_SENDER_ADDRESS: str = get_env("EMAIL_SENDER_ADDRESS")
     EMAIL_CONNECTION_STRING: str = get_env("EMAIL_CONNECTION_STRING")
     POLLER_WAIT_TIME: int = 10
