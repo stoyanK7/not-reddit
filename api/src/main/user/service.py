a6e6cc08c62109439b8c439893da7940a75f34a9
stoyanK7 <stoyank127@gmail.com>
2023-04-27 18:09:35 +0200

    Create UserService

---
+++ api/src/main/user/service.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from src.main.amqp_publisher import AmqpPublisher
+from src.main.user.settings import settings
+
+
+class UserService(FastAPI):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.successful_registration_amqp_publisher = None
+        self.initialize_amqp_publishers()
+
+    def initialize_amqp_publishers(self):
+        self.successful_registration_amqp_publisher = AmqpPublisher(
+            settings.AMQP_URL,
+            exchange_name=settings.AMQP_SUCCESSFUL_REGISTRATION_EXCHANGE_NAME,
+        )
