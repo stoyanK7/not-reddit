5004928fc97871dcadfda3505821cb5102ee0ee6
stoyanK7 <stoyank127@gmail.com>
2023-04-27 15:23:38 +0200

    Add SERVICE_PREFIX in post service settings

--- api/src/main/post/settings.py
+++ api/src/main/post/settings.py
@@ -2,7 +2,8 @@ from src.main.env import get_env
 
 
 class PostServiceSettings:
-    AMQP_URL = get_env("AMQP_URL")
+    SERVICE_PREFIX: str = "/api/post"
+    AMQP_URL: str = get_env("AMQP_URL")
 
 
 settings = PostServiceSettings()
