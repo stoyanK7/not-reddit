e2cbc7ea22eaf1f599541bf9bb89d383fea30207
stoyanK7 <stoyank127@gmail.com>
2023-04-27 18:36:23 +0200

    Add /api/post to test endpoints

--- api/src/test/post/test_post.py
+++ api/src/test/post/test_post.py
@@ -6,7 +6,7 @@ def test_get_posts_length(client, session, insert_mock_text_posts):
     """Assert that posts length is 10."""
     insert_mock_text_posts(12, session=session)
 
-    response = client.get("/")
+    response = client.get("/api/post")
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()) == 10
@@ -16,7 +16,7 @@ def test_get_posts_pagination(client, session, insert_mock_text_posts):
     """Assert that pagination works."""
     insert_mock_text_posts(23, session=session)
 
-    response = client.get("/?page=2")
+    response = client.get("/api/post?page=2")
 
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()) == 3
@@ -25,20 +25,20 @@ def test_get_posts_pagination(client, session, insert_mock_text_posts):
 def test_get_post(client, session, insert_mock_text_posts):
     """Assert that post can be fetched by id."""
     post = insert_mock_text_posts(1, session=session)[0]
-    response = client.get(f"/{post.id}")
+    response = client.get(f"/api/post/{post.id}")
 
     assert response.status_code == status.HTTP_200_OK
     assert {"id", "title", "body", "posted_at"} == set(response.json().keys())
 
 
-def test_create_text__post(client, remove_json_fields):
+def test_create_text_post(client, remove_json_fields):
     """Assert that post is created."""
     body = {
         "title": "Test post",
         "body": "Test body",
     }
 
-    response = client.post("/text", json=body)
+    response = client.post("/api/post/text", json=body)
 
     assert response.status_code == status.HTTP_201_CREATED
     assert {"id", "title", "body", "posted_at"} == set(response.json().keys())
@@ -53,7 +53,7 @@ def test_create_text__post(client, remove_json_fields):
 def test_delete_post(client, session, insert_mock_text_posts):
     """Assert that post is deleted."""
     post = insert_mock_text_posts(1, session=session)[0]
-    response = client.delete(f"/{post.id}")
+    response = client.delete(f"/api/post/{post.id}")
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert session.query(PostModel).filter_by(id=post.id).first() is None
