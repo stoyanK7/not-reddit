619f888277a844c7203956dd9ddba2f41eb0f567
stoyanK7 <stoyank127@gmail.com>
2023-04-30 15:39:00 +0200

    Do not throw error when posts cannot be fetched

--- ui/src/main/pages/index.jsx
+++ ui/src/main/pages/index.jsx
@@ -1,5 +1,5 @@
 import { AuthenticatedTemplate } from "@azure/msal-react";
-import { toast } from "react-toast";
+import { toast, ToastContainer } from "react-toast";
 import useSWR from "swr";
 
 import CreatePost from "@/components/CreatePost";
@@ -13,17 +13,21 @@ export default function Home() {
 
     if (error) {
         toast.error("Failed to load latest posts");
-    };
+    }
     if (isLoading) return <div>loading...</div>;
 
     return (
         <main
             className="w-1/2 mx-auto min-w-fit my-2 mt-24">
+            <ToastContainer
+                delay={8000} />
             <AuthenticatedTemplate>
                 <CreatePost />
             </AuthenticatedTemplate>
-            <PostList
-                posts={posts} />
+            {posts &&
+                <PostList
+                    posts={posts} />
+            }
         </main>
     );
 }
