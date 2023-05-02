84c0b3c21b42dbfc53a0c9071a7ee3f930fc402f
stoyanK7 <stoyank127@gmail.com>
2023-05-02 14:40:39 +0200

    Add function for inserting user and oid

--- api/src/main/comment/crud.py
+++ api/src/main/comment/crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from src.main.comment.schema import CommentCreate
-from src.main.comment.model import Comment as CommentModel
+from src.main.comment.model import Comment as CommentModel, User as UserModel
 
 
 def get_10_comments(db: Session, page: int = 0):
@@ -22,3 +22,11 @@ def delete_comment(db: Session, comment_id: int):
     db.query(CommentModel).filter(CommentModel.id == comment_id).delete()
     db.commit()
     return
+
+
+def insert_user(db: Session, username: str, oid: str):
+    db_user = UserModel(username=username, oid=oid)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
