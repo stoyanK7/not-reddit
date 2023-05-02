65daea2da55555609d18f483b9e87da0d9068d0a
stoyanK7 <stoyank127@gmail.com>
2023-05-02 18:16:18 +0200

    Show message when there are no posts

--- ui/src/main/components/PostList.jsx
+++ ui/src/main/components/PostList.jsx
@@ -6,6 +6,7 @@ export default function PostList({ posts, mutate }) {
     return (
         <div
             className="flex flex-col gap-2">
+            {posts.length === 0 && <span>There are no posts to show.</span>}
             {posts.map((post) => (
                 <Link
                     key={post.id}
