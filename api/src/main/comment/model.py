5700484eecec597fe4a637b145f91729b9a5c90b
stoyanK7 <stoyank127@gmail.com>
2023-05-02 14:37:23 +0200

    Add table for storing users and oids in comments microservice

--- api/src/main/comment/model.py
+++ api/src/main/comment/model.py
@@ -12,3 +12,10 @@ class Comment(Base):
     user_id = Column(Integer)
     post_id = Column(Integer)
     commented_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class User(Base):
+    __tablename__ = "comment_users_oid"
+
+    oid = Column(String, primary_key=True, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
