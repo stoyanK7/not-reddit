8af75667958ab9ecff5e38aa2dd0b048c6ef68f3
stoyanK7 <stoyank127@gmail.com>
2023-05-06 13:04:54 +0200

    Prepare connection for vote casting

--- api/src/main/vote/lifespan.py
+++ api/src/main/vote/lifespan.py
@@ -19,6 +19,10 @@ async def lifespan(app: VoteService) -> Lifespan[AppType]:
     await task
     task = loop.create_task(app.comment_created_amqp_consumer.consume(loop))
     await task
+    task = loop.create_task(app.post_vote_casted_amqp_publisher.prepare_connection(loop))
+    await task
+    task = loop.create_task(app.comment_vote_casted_amqp_publisher.prepare_connection(loop))
+    await task
     yield
 
 
