1bab6df9540ab84b0cfea3f94580733d41e86bab
stoyanK7 <stoyank127@gmail.com>
2023-05-07 17:52:03 +0200

    Update nginx config

--- api/src/main/auth/util.py
+++ api/src/main/auth/util.py
@@ -10,7 +10,11 @@ def configure_cors(app: FastAPI) -> None:
     if settings.CORS_ORIGINS:
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=[str(origin) for origin in settings.CORS_ORIGINS],
+            allow_origins=[
+                'http://localhost:8000',
+                "http://localhost:8080",
+                "http://localhost:3000",
+            ],
             allow_credentials=True,
             allow_methods=['*'],
             allow_headers=['*'],
