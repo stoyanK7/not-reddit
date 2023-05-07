f0c140891d36783b833d28b195f64ad2271b0237
stoyanK7 <stoyank127@gmail.com>
2023-05-07 20:14:27 +0200

    Remove username from localstorage on logout

--- ui/src/main/components/LogoutButton.jsx
+++ ui/src/main/components/LogoutButton.jsx
@@ -6,6 +6,7 @@ export default function LogoutButton() {
     async function logout() {
         await msalInstance.logoutPopup();
         toast.success("Logged out successfully.");
+        localStorage.removeItem("username");
     }
 
     return (
