d748cab9fbc3df2b71b36ce2ac0066abaa2d9a19
stoyanK7 <stoyank127@gmail.com>
2023-05-10 13:38:50 +0200

    Give deployment files more-explicit names

--- .dev/create-kubernetes-databases.sh
+++ .dev/create-kubernetes-databases.sh
@@ -5,14 +5,14 @@ source util.sh
 assert_in_api_dir
 
 # User
-kubectl apply -f src/main/user/k8s/database.yml
+kubectl apply -f src/main/user/k8s/user-database-deployment.yml
 
 # Post
-kubectl apply -f src/main/post/k8s/database.yml
+kubectl apply -f src/main/post/k8s/post-database-deployment.yml
 
 # Comment
-kubectl apply -f src/main/comment/k8s/database.yml
+kubectl apply -f src/main/comment/k8s/comment-database-deployment.yml
 
 # Vote
-kubectl apply -f src/main/vote/k8s/database.yml
+kubectl apply -f src/main/vote/k8s/vote-database-deployment.yml
 
