69913b6fe1c518cf73e8e92a3d0012f955973e82
stoyanK7 <stoyank127@gmail.com>
2023-05-10 14:44:20 +0200

    Add not-reddit-ui azure url to cors settings

--- api/src/main/shared/cors/settings.py
+++ api/src/main/shared/cors/settings.py
@@ -6,6 +6,9 @@ class CorsSettings(BaseSettings):
         'http://localhost:8000',
         "http://localhost:8080",
         "http://localhost:3000",
+        "http://notredditui.switzerlandnorth.cloudapp.azure.com",
+        "http://notredditui.switzerlandnorth.cloudapp.azure.com:3000",
+        "http://notredditapi.switzerlandnorth.cloudapp.azure.com",
     ]
 
 
