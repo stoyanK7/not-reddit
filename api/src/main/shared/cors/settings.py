2f49c24036d6482ce24d96c7ac3034ea9126a571
stoyanK7 <stoyank127@gmail.com>
2023-05-16 14:00:57 +0200

    Accept default value when getting and environment variable

--- api/src/main/shared/cors/settings.py
+++ api/src/main/shared/cors/settings.py
@@ -6,7 +6,7 @@ from src.main.shared.env import get_env
 
 
 class CorsSettings(BaseSettings):
-    CORS_ALLOWED_ORIGINS: list[str] = json.loads(get_env("CORS_ALLOWED_ORIGINS"))
+    CORS_ALLOWED_ORIGINS: list[str] = json.loads(get_env("CORS_ALLOWED_ORIGINS", '["*"]'))
 
 
 settings = CorsSettings()
--- api/src/main/shared/env.py
+++ api/src/main/shared/env.py
@@ -1,14 +1,15 @@
 import os
 
 
-def get_env(key: str) -> str:
+def get_env(key: str, default_value="") -> str:
     """
     Gets environment variable.
 
     Parameters:
         key (str): Environment variable name.
+        default_value (str): Default value if environment variable is not set.
 
     Returns:
         str: Environment variable value.
     """
-    return os.environ.get(key, "")
+    return os.environ.get(key, default_value)
