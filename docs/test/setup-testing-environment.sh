3438796ae02df0e5cdbb42da7c662aa02dea2ead
stoyanK7 <stoyank127@gmail.com>
2023-05-23 11:29:02 +0200

    Fix indentation and wrong request

--- docs/test/setup-testing-environment.sh
+++ docs/test/setup-testing-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script is expected to be run from root folder of repository.
 
-. ./.dev/util.sh
+source ./.dev/util.sh
 
 checkForVariable "JWT_TOKEN"
 checkForVariable "API_URL"
@@ -20,22 +20,22 @@ curl -XPOST \
 # Create 6 text posts
 for i in 1 2 3 4 5 6
 do
-  curl -XPOST \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  -H "Content-type: application/json" \
-  -d "{
-    \"title\": \"$i: Lorem ipsum 1234567890!@#$%^&*()_+{}:<>\",
-    \"body\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"
-  }" \
-  "$API_URL/api/post/text"
+  curl -s -XPOST \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    -H "Content-type: application/json" \
+    -d "{
+      \"title\": \"$i: Lorem ipsum 1234567890!@#$%^&*()_+{}:<>\",
+      \"body\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"
+    }" \
+    "$API_URL/api/post/text" | jq "."
 done
 
 # Create 4 media posts
 for i in 1 2 3 4
 do
-  curl -XPOST \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  -F "title=$i: Lorem ipsum 1234567890!@#$%^&*()_+{}:<>" \
-  -F "body=@docs/test/img/$i.png" \
-  "$API_URL/api/post/media"
+  curl -s -XPOST \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    -F "title=$i: Lorem ipsum 1234567890!@#$%^&*()_+{}:<>" \
+    -F "file=@docs/test/img/$i.png" \
+    "$API_URL/api/post/media" | jq "."
 done
