068b5dfd0cb501df3fe99b8173d6e8c504217ee4
stoyanK7 <stoyank127@gmail.com>
2023-05-24 12:23:30 +0200

    Implement user deletion along with GDPR warnings

--- ui/src/main/pages/auth.jsx
+++ ui/src/main/pages/auth.jsx
@@ -8,7 +8,6 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { toast } from "react-toast";
 
-import LogoutButton from "@/components/LogoutButton";
 import { loginRequest, msalInstance } from "@/utils/authConfig";
 import buildAuthorizationHeader from "@/utils/buildAuthorizationHeader";
 import fromApi from "@/utils/fromApi";
@@ -20,6 +19,7 @@ export default function Auth() {
     const isAuthenticated = accounts.length > 0;
     const [hasUserConsented, setHasUserConsented] = useState(false);
     const [isRegistered, setIsRegistered] = useState(false);
+    const [userWantsToDeleteAccount, setUserWantsToDeleteAccount] = useState(false);
 
     useEffect(() => {
         async function checkRegistrationStatus() {
@@ -111,6 +111,28 @@ export default function Auth() {
         }
     }
 
+    async function handleDeleteAccount() {
+        const accessToken = await getAccessToken();
+        if (accessToken === null) {
+            toast.error("Failed to get your access token");
+            return;
+        }
+
+        const res = await fetch(fromApi("/api/user"), {
+            method: "DELETE",
+            credentials: "include",
+            headers: buildAuthorizationHeader(accessToken),
+        });
+
+        if (!res.ok) {
+            toast.error("Failed to delete your account");
+            return;
+        }
+
+        toast.success("Account deleted successfully");
+        await msalInstance.logoutPopup();
+    }
+
     return (
         <main
             className="flex flex-col w-screen h-screen justify-center items-center">
@@ -121,13 +143,28 @@ export default function Auth() {
                 <AuthenticatedTemplate>
                     {isAuthenticated && !isRegistered && (
                         <>
+                            <h2
+                                className="text-4xl">Privacy policy</h2>
+                            <h3
+                                className="text-2xl">Account registration</h3>
                             <p
-                                className="italic underline text-center">
+                                className="italic underline">
                                 By registering, you consent to the storage of your email for
                                 identification
                                 purposes. We prioritize your privacy and will only use your email
                                 for
                                 essential account-related communication.
+                            </p>
+                            <h3
+                                className="text-2xl">Account deletion</h3>
+                            <p
+                                className="underline italic">
+                                In accordance with GDPR guidelines, when a user requests account
+                                deletion, we ensure complete erasure of their personal data. This
+                                includes removing all posts, comments, and votes associated with
+                                their account. By taking these measures, we respect the user&apos;s
+                                right to be forgotten and prioritize the protection of their
+                                privacy.
                             </p>
                             <label>
                                 <input
@@ -161,10 +198,39 @@ export default function Auth() {
                                     Go to home page
                                 </button>
                             </Link>
-                            <span>or..</span>
+                            {!userWantsToDeleteAccount && (
+                                <button
+                                    className="p-2 rounded-sm bg-red-500 text-white"
+                                    onClick={() => setUserWantsToDeleteAccount(true)}>
+                                    Delete my account
+                                </button>
+                            )}
+                            {userWantsToDeleteAccount && (
+                                <>
+                                    <button
+                                        className="p-2 rounded-sm bg-green-500 text-white"
+                                        onClick={() => setUserWantsToDeleteAccount(false)}>
+                                        No, don&apos;t delete my account!
+                                    </button>
+                                    <p
+                                        className="underline italic">
+                                        In accordance with GDPR guidelines, when a user requests
+                                        account
+                                        deletion, we ensure complete erasure of their personal data.
+                                        This
+                                        includes removing all posts, comments, and votes associated
+                                        with
+                                        their account.
+                                    </p>
+                                    <button
+                                        className="p-2 rounded-sm bg-red-500 text-white"
+                                        onClick={handleDeleteAccount}>
+                                        Yes, delete my account!
+                                    </button>
+                                </>
+                            )}
                         </>
                     )}
-                    <LogoutButton />
                 </AuthenticatedTemplate>
                 <UnauthenticatedTemplate>
                     <span
